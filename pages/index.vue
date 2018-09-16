f668a440334adebca7c00d2b35d69da81a0fa264
uruha <kochmusica@gmail.com>
2018-09-16 13:22:47 +0900

    change: text

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         koch-sample
       </h1>
       <h2 class="subtitle">
-        My dandy Nuxt.js project
+        For study and development for GAE.
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
