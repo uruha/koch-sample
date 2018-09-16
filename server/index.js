e5ee412d7f58c5b725be02452fed20869defeecd
uruha <kochmusica@gmail.com>
2018-09-16 22:17:43 +0900

    change: refactor

--- server/index.js
+++ server/index.js
@@ -1,26 +1,9 @@
 const Koa = require('koa');
-// const { Nuxt, Builder } = require('nuxt');
 const { Nuxt } = require('nuxt');
-
-// const app = new Koa();
-// const host = process.env.HOST || 'localhost';
-// const port = process.env.PORT || 8080;
-
-// Import and Set Nuxt.js options
-// let config = require('../nuxt.config.js');
-// config.dev = !(app.env === 'production');
-
 async function start() {
   const app = new Koa();
   const config = require('../nuxt.config.js');
-  // Instantiate nuxt.js
   const nuxt = new Nuxt(config);
-
-  // Build in development
-  // if (config.dev) {
-  //   const builder = new Builder(nuxt);
-  //   await builder.build();
-  // }
 
   app.use(async (ctx, next) => {
     try {
@@ -31,13 +14,12 @@ async function start() {
       ctx.app.emit('error', err);
     }
 
-    ctx.status = 200; // koa defaults to 404 when it sees that status is unset
+    ctx.status = 200;
     ctx.set('Cache-Control', 'public, max-age=10800');
     return new Promise((resolve, reject) => {
       ctx.res.on('close', resolve);
       ctx.res.on('finish', resolve);
       nuxt.render(ctx.req, ctx.res, promise => {
-        // nuxt.render passes a rejected promise into callback on error.
         promise.then(resolve).catch(reject);
       });
     });
